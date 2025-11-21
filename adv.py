e321c27168b7e5e35a3d5283659c3226715b235c
koushikarnuri <146161200+koushikarnuri@users.noreply.github.com>
2025-11-21 21:55:25 +0530

    adv.py

--- adv.py
+++ adv.py
@@ -9,11 +9,12 @@ model=joblib.load(open("linear_regression_model (1).pkl",'rb'))
 st.title('sales prediction app')
 
 #input
-TV=st.number_input("TV Adv budget",min_value=0.0)
+Tv=st.number_input("Tv Adv budget",min_value=0.0)
 Radio=st.number_input("Radio Adv budget",min_value=0.0)
 Newspaper=st.number_input("Newspaper Adv budget",min_value=0.0)
 #pred
 if st.button('predict sales'):
 	input_data=np.array([[TV,Radio,Newspapers]])
 	prediction=model.predict(input_data)[0]
+
 	st.success('predict sales:{prediction:.2f}')
